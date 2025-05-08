81464954d99fa12f43c3f5e332b7118b62db55df
YoussefBahloul <99575696+YoussefBahloul512@users.noreply.github.com>
2025-05-09 00:20:54 +0300

    Add files via upload
    This is a small task from "Cross Code" Youtuber and this is the video: https://www.youtube.com/watch?v=WVCzC8TFrQQ
    
    And this is my Answer NO AI just some search as he told me

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('button');
+button.addEventListener('mouseover', function () {
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+});
